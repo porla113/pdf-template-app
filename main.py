9ddb9c70061fc54a89a9ea531efb159820e74fd5
Por Cho <porla113@gmail.com>
2025-05-28 14:11:03 +0700

    install pandas and implement a script to create pdf from topics.csv

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from fpdf import FPDF
+import pandas as pd
+
+df = pd.read_csv("topics.csv")
 
 pdf_doc = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf_doc.add_page()
+for index, row in df.iterrows():
+    # print(row["Topic"])
+    page = 0
 
-pdf_doc.set_font(family="Times", style="B", size=12)
-pdf_doc.cell(w=20, h=12, txt="Hi There!", align="L", ln=1, border=1)
-pdf_doc.cell(w=0, h=12, txt="Nice to create PDF", align="L", ln=1, border=1)
+    while page < row["Pages"]:
+        pdf_doc.add_page()
 
-pdf_doc.add_page()
-
-pdf_doc.set_font(family="Times", style="B", size=12)
-pdf_doc.cell(w=20, h=12, txt="Hi page 2!", align="L", ln=1, border=1)
-pdf_doc.cell(w=0, h=12, txt="Nice to create page 2", align="L", ln=1, border=1)
+        pdf_doc.set_font(family="Times", style="B", size=24)
+        pdf_doc.set_text_color(100,100,100)
+        pdf_doc.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+        pdf_doc.line(10,24,200,24)
+        page += 1
 
 pdf_doc.output("output.pdf")
