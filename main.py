ceef84bcb0da932a8d481ec526db0c30b9b6ef2e
Por Cho <porla113@gmail.com>
2025-05-28 15:39:24 +0700

    add header and footer

--- main.py
+++ main.py
@@ -4,18 +4,35 @@ import pandas as pd
 df = pd.read_csv("topics.csv")
 
 pdf_doc = FPDF(orientation="P", unit="mm", format="A4")
+pdf_doc.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
-    # print(row["Topic"])
-    page = 0
 
-    while page < row["Pages"]:
+    # Add a topic page
+    pdf_doc.add_page()
+
+    # Header
+    pdf_doc.set_font(family="Times", style="B", size=24)
+    pdf_doc.set_text_color(100,100,100)
+    pdf_doc.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+    pdf_doc.line(10,24,200,24)
+
+    pdf_doc.ln(251)
+
+    # Footer
+    pdf_doc.line(10,275,200,275)
+    pdf_doc.set_font(family="Times", style="I", size=12)
+    pdf_doc.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1, border=0)
+
+    for i in range(row["Pages"] - 1):
         pdf_doc.add_page()
 
-        pdf_doc.set_font(family="Times", style="B", size=24)
-        pdf_doc.set_text_color(100,100,100)
-        pdf_doc.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-        pdf_doc.line(10,24,200,24)
-        page += 1
+        pdf_doc.ln(263) 
+
+        # Footer
+        pdf_doc.line(10,275,200,275)
+        pdf_doc.set_font(family="Times", style="I", size=12)
+        pdf_doc.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1, border=0)
+
 
 pdf_doc.output("output.pdf")
