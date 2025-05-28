2e423bf00861cc18cf86b9bccf5dbdb61a8ed275
Por Cho <porla113@gmail.com>
2025-05-28 16:06:47 +0700

    add lines

--- main.py
+++ main.py
@@ -17,9 +17,17 @@ for index, row in df.iterrows():
     pdf_doc.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
     pdf_doc.line(10,24,200,24)
 
+    # Draw lines
+    pdf_doc.set_draw_color(200, 200, 220)
+    line_height_start = 34
+    for i in range(0, 24):
+        line_height = line_height_start + 10 * i
+        pdf_doc.line(10, line_height, 200, line_height)
+
     pdf_doc.ln(251)
 
     # Footer
+    pdf_doc.set_draw_color(0, 0, 0)
     pdf_doc.line(10,275,200,275)
     pdf_doc.set_font(family="Times", style="I", size=12)
     pdf_doc.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1, border=0)
@@ -27,9 +35,17 @@ for index, row in df.iterrows():
     for i in range(row["Pages"] - 1):
         pdf_doc.add_page()
 
+        # Draw lines
+        pdf_doc.set_draw_color(200, 200, 220)
+        line_height_start = 24
+        for i in range(0, 25):
+            line_height = line_height_start + 10 * i
+            pdf_doc.line(10, line_height, 200, line_height)
+
         pdf_doc.ln(263) 
 
         # Footer
+        pdf_doc.set_draw_color(0, 0, 0)
         pdf_doc.line(10,275,200,275)
         pdf_doc.set_font(family="Times", style="I", size=12)
         pdf_doc.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1, border=0)
